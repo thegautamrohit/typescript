1dea4df76dfef4a7b0fb52fa7dfcc4fde4ece718
thegautamrohit <rgautam1997@gmail.com>
2023-08-20 00:16:46 +0530

    tuples

---
+++ basics/tuples/tuples.ts
@@ -0,0 +1,17 @@
+//Tuples is a way of determining the data types of the elements in the array as per their order
+// For the below example,  it signifies that this array can have 3 elements with the
+// given data types and should in the same order also
+
+let user: [string, number, boolean];
+
+user = ["Rohit", 26, true];
+
+
+
+//However, there is an edge case below, we can still use the array methods to 
+// change the elements of te array against the pre-set rules that we defined usinf tuples
+
+user.push('true')
+
+
+export {};
