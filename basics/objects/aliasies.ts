168c7bbdc23a7dc33f49d1a50138630a60b25659
thegautamrohit <rgautam1997@gmail.com>
2023-08-19 20:26:19 +0530

    type aliases

---
+++ basics/objects/aliasies.ts
@@ -0,0 +1,9 @@
+type User = {
+  name: string;
+  email: string;
+  isActive: boolean;
+};
+
+function createUser(user: User): User {
+  return { name: "Rohit", email: "r@g.com", isActive: true };
+}
