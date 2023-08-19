cc20dcb1da4de73e6b964b1387f895823262ce71
thegautamrohit <rgautam1997@gmail.com>
2023-08-19 23:35:14 +0530

    array

---
+++ basics/array/array.ts
@@ -0,0 +1,7 @@
+// Below are the examples of how we can define a array
+// we can use either of them
+
+
+const heroes: string[] = [];
+// const heroPowers: number[] = []
+const heroPowers: Array<number> = [];
